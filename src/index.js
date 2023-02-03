375f2f587fb3091aa0696ee9b71bc15ed6af7ca4
Luis <fl337789@uaeh.edu.mx>
2023-02-02 22:29:48 -0600

    Creamos el schema de usauiro y preparamos la API para recivir y enviar los datos a MONGODB atlas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 //levantamos el servidor
 const express = require('express'); //cargamos el modulo expressJS
+const mogoose = require("mongoose");//cargamos el modulo mongoose para la conexxion a ala base de datos
+require("dotenv").config();
+const userRoutes = require("./routes/user");
 
 const app = express();//aplicacion
 const port = process.env.PORT ||9000;//si se despliga en un servidor externo toma su puerto sino, ocupa el 9000
+
+//middleware //codigo antes de abrir el cuerpo del api
+app.use(express.json());
+app.use("/api", userRoutes);
+
+//routes 
+app.get("/", (req, res)=>{
+    res.send("bienvenido a mi API");
+});
+
+//conexion a mongodb
+mogoose
+.connect(process.env.MONGODB_URI)//utilizamos nuestra variable de enterono)
+.then(()=>console.log("Conectado a MongoDB atlas"))
+.catch((error) => console.error(error));
 
 app.listen(port, ()=> console.log("el servidor esta escuchando en la puerta: " + port));//el servidor va a escruchar en la puerta 9000
 
