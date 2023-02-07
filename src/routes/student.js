b1439215f5f9feebadc8d23651838e67cf0a1c5e
Luis <fl337789@uaeh.edu.mx>
2023-02-06 20:04:12 -0600

    Creamos las operaciones basicas para los estudiantes

--- src/routes/student.js
+++ src/routes/student.js
@@ -13,8 +13,42 @@ router.post('/students' ,(req, res) => {
         .save()
         .then((data)=> res.json(data))
         .catch((error)=> res.json({mesagge: error}));
-
+//obtener todos los usuarios o listar
 });
+router.get('/students', (req, res) =>{
+    StudentSchema
+        .find() //en lugar de guardar buscamos
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message: error}));
+});
+//obtener un usuario
+router.get('/students/:id', (req, res) =>{
+    const { id } = req.params;
+    StudentSchema
+        .findById(id) //buscamos por id
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message: error}));
+});
+//actulizar usuario
+router.put('/students/:id', (req, res) =>{
+    const { id } = req.params;
+    const { name, age, active} = req.body;
+    StudentSchema
+        .updateOne({ _id: id },{ $set: {name, age, active} }) //buscamos por id
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message: error}));
+});
+
+//borrar un usuario
+router.delete('/students/:id', (req, res) =>{
+    const { id } = req.params;
+    StudentSchema
+        .remove({ _id: id }) //buscamos por id
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message: error}));
+});
+
+
 
 
 module.exports = router;
