9ead74f58dd1789cc613b95275c670afc5ff0879
Luis <fl337789@uaeh.edu.mx>
2023-02-10 17:37:18 -0600

    Creamos las funciones para leer y agregar datos a la BD mediante JS y la API

--- src/Front/app.js
+++ src/Front/app.js
@@ -1,10 +1,12 @@
+//URL de la API local
 const API_URL = "http://localhost:9000/api/";
 var classrooms = [];
 
+//esperamos hasta que el dom carge el contenido para mandar a traer los datos BD
 window.addEventListener('DOMContentLoaded', ()=>{
     getClassrooms();
 })
-
+//recuperamos todos los datos de la bd
 const getClassrooms = () => {
     fetch(`${API_URL}/classrooms`)
     .then(response => response.json())
@@ -32,21 +34,42 @@ const renderResult = (classrooms) =>{
     })
     classroomsList.innerHTML = listHTML;
 }
-
-
+//agregamos una nueva clase a la BD
 const createClassRoom = () =>{
     const formData = new FormData(document.querySelector("#formulario"));
+    
+    if(!formData.get('class').length || !formData.get('order').length || !formData.get('numeroofstudents').length 
+    || !formData.get('active').length ||!formData.get('liststudents').length )
+    {
+        document.querySelector('#alert').innerHTML = "* Todos los campos son obligatorios";
+        return;
+    }
+    document.querySelector("#alert").innerHTML = '';
 
     const classroom = {
-        Class: formData.get("#class"),
-        Order: formData.get("#order"),
-        numberOfStudents: formData.get("#numberofstudents"),
-        active: formData.get("#active"),
-        ListStudents: formData.get("#listofstudents")
+        Class: formData.get("class"),
+        Order: formData.get("order"),
+        numberOfStudents: formData.get("numeroofstudents"),
+        active: formData.get("active"),
+        ListStudents: formData.get("liststudents"),
     }
-
     console.log(classroom);
+
+    fetch(`${API_URL}/classrooms`, {
+        method: 'POST',
+        body: JSON.stringify(classroom),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(response => {
+        console.log(response);
+        getClassrooms();
+    })
 }
+
+
 
 
 
